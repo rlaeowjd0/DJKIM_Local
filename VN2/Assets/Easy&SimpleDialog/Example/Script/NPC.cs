0cd35a287d0dbac53f2b54f562a95e6fe25cfe31
DESKTOP-NE374FM\lotte <ahwkfl1@gmail.com>
2018-05-17 10:20:01 +0900

    비주얼노벨 자료

---
+++ VN2/Assets/Easy&SimpleDialog/Example/Script/NPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameObject.Find("Canvas").transform.Find("Dialog").gameObject.SetActive(true);
+        }
+    }
+}
